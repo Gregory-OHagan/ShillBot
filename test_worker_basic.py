a94c60070cd62e7f58890033ab8ec6e04981d6b1
Gregory O'Hagan <gregoryrohagan@gmail.com>
2017-07-28 01:11:26 -0700

    test 7

--- test_worker_basic.py
+++ test_worker_basic.py
@@ -35,8 +35,7 @@ class TestWorkerBasic(unittest.TestCase):
 
         with codecs.open(file_path, encoding='utf-8') as f:
             text = f.read()
-        temp = str(text)
-        results, next_page = worker.parse_text(str(text).strip().replace('\r\n', ''))
+        results, next_page = worker.parse_text(text.encode("utf-8".strip().replace('\r\n', ''))
 
         self.assertGreater(len(results), 0)     # Check that results are returned
         self.assertEqual(len(results[0]), 3)    # Check that results are in triplets (check formatting)
